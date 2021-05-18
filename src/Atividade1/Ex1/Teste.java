10e6a6fe97c6c5a85619f687adabd4864155e65d
tiagoriosrocha <tiagoriosrocha@gmail.com>
2021-05-18 14:43:48 -0300

    Adicionei novo código

--- src/Atividade1/Ex1/Teste.java
+++ src/Atividade1/Ex1/Teste.java
@@ -3,5 +3,10 @@ package Atividade1.Ex1;
 public class Teste {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
